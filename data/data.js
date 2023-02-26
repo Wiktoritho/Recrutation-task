dbc07fe72b77f7815118da85e9a6db9c45988af2
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-02-26 17:20:44 +0100

    Create data.js
    Adding database data.js

---
+++ data/data.js
@@ -0,0 +1,35 @@
+export var offers = [
+    {
+        id: 0,
+        name: "3 Bed Apt For Sale in Dubai Marina",
+        price: "65K",
+        currency: "AED",
+        image: "offer_0.png",
+        ribbon: "-15%",
+    },
+    {
+        id: 1,
+        name: "2 Bed Apt For Sale in Dubai Marina",
+        price: "60K",
+        currency: "AED",
+        image: "offer_1.png",
+        ribbon: "-16%",
+    },
+    {
+        id: 2,
+        name: "2 Bed Apt For Sale in Al Barsha",
+        price: "50K",
+        currency: "AED",
+        image: "offer_2.png",
+        ribbon: "-10%",
+    },
+    {
+        id: 3,
+        name: "3 Bed Apt For Sale in Al Barsha",
+        price: "80K",
+        currency: "AED",
+        image: "offer_0.png",
+        ribbon: "-12%",
+    }
+
+]

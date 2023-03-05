3c984d31efcf698176598537f551011403a1ab3a
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-03-05 17:17:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { offers } from "../data/data.js";
-
 const slides = document.querySelectorAll(".container");
 const description = document.querySelectorAll(".container__description");
 const price = document.querySelectorAll(".container__price");
@@ -8,80 +7,70 @@ const box = document.querySelectorAll(".container__box");
 const image = document.querySelectorAll(".container__image");
 const button = document.querySelectorAll(".container__button");
 const navlinks = document.querySelectorAll(".list__dot");
-const arrow = document.querySelectorAll(".arrow");
-let currentSlide = 0;
+let timerContainer = null;
+let timerBox = null;
+
 
 function createDataFromObject() {
-  for (let i = 0; i < description.length; i++) {
+  for (let i = 0; i < box.length; i++) {
     description[i].innerHTML = offers[i].name;
     price[i].innerHTML = offers[i].currency + " " + offers[i].price;
     discount[i].innerHTML = offers[i].ribbon;
-    image[i].setAttribute("src", `images/${offers[i].image}`);
+    image[i].setAttribute("src", `../images/${offers[i].image}`);
   }
 }
 
-function createHoverEffect() {
+function cutText() {
+  for (let z = 0; z < description.length; z++) {
+    if (description[z].innerText.length > 40) {
+      description[z].innerText = description[z].innerText.substring(0, 39) + "...";
+    }
+  }
+}
+
+function setTimer() {
+  timerContainer = setInterval(changeSlideAutomatic, 5000);
+  timerBox = setInterval(changeOfferAutomatic, 2500);
+}
+
+function changeOfferAutomatic() {
   for (let j = 0; j < box.length; j++) {
-    box[j].addEventListener("mouseover", () => {
-      image[j].style.opacity = "0.5";
-      button[j].style.cursor = "pointer";
-      button[j].style.display = "block";
-      image[j].style.transition = "0.5s";
-      button[j].style.transition = "0.5s";
+    image[j].classList.toggle("active__image");
+    button[j].classList.toggle("active__button");
+  }
+}
+
+function changeSlideAutomatic() {
+  for (let jj = 0; jj < slides.length; jj++) {
+    slides[jj].classList.toggle("none");
+    navlinks[jj].classList.toggle("active");
+  }
+}
+
+function stopSlideAutomatic() {
+  for (let iii = 0; iii < box.length; iii++) {
+    box[iii].addEventListener("mouseover", () => {
+      clearInterval(timerContainer);
+      clearInterval(timerBox);
+      for (let jjj = 0; jjj < box.length; jjj++) {
+        if (image[jjj].classList.contains("active__image")) {
+          image[jjj].classList.remove("active__image");
+          button[jjj].classList.remove("active__button");
+        }
+      }
+    });
+    box[iii].addEventListener("mouseout", () => {
+      image[0].classList.add("active__image");
+      button[0].classList.add("active__button");
+      image[2].classList.add("active__image");
+      button[2].classList.add("active__button");
+      setTimer();
     });
   }
 }
 
-function removeHoverEffect() {
-  for (let ii = 0; ii < box.length; ii++) {
-    box[ii].addEventListener("mouseout", () => {
-      image[ii].style.opacity = "1";
-      button[ii].style.cursor = "arrow";
-      button[ii].style.display = "none";
-    });
-  }
-}
 
-function changeSlide(n) {
-  if (n >= slides.length) {
-    n = 0;
-  }
-  if (n < 0) {
-    n = slides.length - 1;
-  }
-  slides[currentSlide].classList.toggle("none");
-  navlinks[currentSlide].classList.toggle("active");
-  slides[n].classList.toggle("none");
-  navlinks[n].classList.toggle("active");
-  currentSlide = n;
-  console.log(currentSlide);
-  for (let jj = 0; jj < arrow.length; jj++) {
-    if (currentSlide === 1) {
-      arrow[0].classList.remove("none");
-      arrow[1].classList.add("none");
-    } else {
-      arrow[0].classList.add("none");
-      arrow[1].classList.remove("none");
-    }
-  }
-}
-
-navlinks.forEach((bullet, bulletIndex) => {
-  bullet.addEventListener("click", () => {
-    if (currentSlide !== bulletIndex) {
-      changeSlide(bulletIndex);
-    }
-  });
-});
-
-arrow.forEach((bullet, bulletIndex) => {
-  bullet.addEventListener("click", () => {
-    if (currentSlide !== bulletIndex) {
-      changeSlide(bulletIndex);
-    }
-  });
-});
-
+setTimer();
+stopSlideAutomatic();
 createDataFromObject();
-createHoverEffect();
-removeHoverEffect();
+cutText();
